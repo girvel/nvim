b31d692004b253bfbbb40edc06057f47373dbcec
Nikita Dobrynin <widauka@ya.ru>
2024-06-16 21:56:17 +0500

    #

--- init.lua
+++ init.lua
@@ -107,6 +107,9 @@ require("mason-lspconfig").setup()
 require("lspconfig").lua_ls.setup({
   settings = {
     Lua = {
+      runtime = {
+        version = "LuaJIT"
+      },
       diagnostics = {
         globals = {"vim", "love"}
       }
