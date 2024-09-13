2e780233031e7f83b6d55691e5cdaefcbe3ac516
girvel <widauka@ya.ru>
2024-09-13 15:51:17 +0500

    #

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -40,15 +40,15 @@ return {
   },
 
   -- fancy tabs
-  {
-    "akinsho/bufferline.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require("bufferline").setup {
-        options = {
-          separator_style = "slant",
-        },
-      }
-    end,
-  },
+  -- {
+  --   "akinsho/bufferline.nvim",
+  --   dependencies = "nvim-tree/nvim-web-devicons",
+  --   config = function()
+  --     require("bufferline").setup {
+  --       options = {
+  --         separator_style = "slant",
+  --       },
+  --     }
+  --   end,
+  -- },
 }
